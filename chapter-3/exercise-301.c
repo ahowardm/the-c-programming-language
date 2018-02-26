453ae864420ad97ae92c31e2a5ba9bc6632fae2c
Andres Howard <ahowardm@gmail.com>
2018-02-26 10:30:38 -0300

    exercise 3.01

--- chapter-3/exercise-301.c
+++ chapter-3/exercise-301.c
@@ -12,7 +12,7 @@ int main() {
   }
   printf("\n");
 
-  printf("%d\n", binsearch_v2(4, v, SIZEARRAY));
+  printf("%d\n", binsearch(4, v, SIZEARRAY));
 }
 
 
@@ -40,12 +40,12 @@ int binsearch_v2(int x, int v[], int n) {
   low = 0;
   high = n - 1;
   mid = (low + high)/2;
-  while(low <= high && x != v[mid]) {
+  while(low < high) {
     mid = (low + high)/2;
     if (x < v[mid])
-      high = mid - 1;
+      high = mid;
     else
       low = mid + 1;
   }
-  return (x == v[mid]) ? mid : -1;
+  return (x == v[low]) ? low : -1;
 }
